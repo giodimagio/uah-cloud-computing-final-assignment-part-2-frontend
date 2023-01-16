32a3117e974cbf32e9c1602ca29087c9fa1c5056
giodimagio <giodimagio@gmail.com>
2023-01-16 23:38:05 +0100

    [svelte] Minimal implementation of the frontend

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Button from "./lib/Button.svelte";
+
+    let endpointMicroservice1 = "http://127.0.0.1:5000/microservice-1";
+    let endpointMicroservice2 = "http://127.0.0.1:5000/microservice-2";
+    let endpointMicroservice3 = "http://127.0.0.1:5000/microservice-3";
+</script>
+
+<main>
+    <h1>UAH - Cloud Computing - Final Assignment - Part 2</h1>
+    <h2>Backend - Flask | Frontend - Svelte</h2>
+    <h3>Click one of the three buttons to check which microservice triggers</h3>
+    <Button endpoint={endpointMicroservice1} componentName="Button1" />
+    <Button endpoint={endpointMicroservice2} componentName="Button2" />
+    <Button endpoint={endpointMicroservice3} componentName="Button3" />
+</main>
+
+<style>
+</style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let endpoint;
+    export let componentName;
+
+    let microserviceVersion;
+    let isLoading = false;
+    let requestFailed = false;
+    let counter = 0;
+
+    async function getMicroservice() {
+        isLoading = true;
+        counter += 1;
+        try {
+            const response = await fetch(endpoint);
+            const data = await response.text();
+            microserviceVersion = data;
+            console.log(microserviceVersion);
+        } catch (error) {
+            requestFailed = true;
+            console.error(error);
+        } finally {
+            isLoading = false;
+        }
+    }
+</script>
+
+<button on:click={getMicroservice} disabled={isLoading}>
+    {isLoading ? "Loading..." : ""}
+    {componentName} clicked {counter}
+    {counter >= 1 ? "times" : "time"}
+</button>
+
+{#if microserviceVersion}
+    <p>{microserviceVersion}</p>
+{:else if isLoading}
+    <p>Wait for microservice response</p>
+{:else if requestFailed}
+    <p>Request failed. Try again please!</p>
+{:else}
+    <p>Press {componentName} to show microservice number & version please!</p>
+{/if}
